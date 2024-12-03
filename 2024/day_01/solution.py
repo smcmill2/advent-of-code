5308a575231e4622f3093c59860b2981ed94ac5e
Sean McMillan <sean.mcmillan@gc.com>
2024-12-03 01:15:13 -0500

    solve: day1, part2

--- 2024/day_01/solution.py
+++ 2024/day_01/solution.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from collections import defaultdict
 from pathlib import Path
 
 
@@ -19,9 +20,23 @@ def compare_lists(list1: list[int], list2: list[int]) -> bool:
     return diff
 
 
+def similarity_score(list1: list[int], list2: list[int]) -> float:
+    list1.sort()
+    list2_counts = defaultdict(int)
+    for _l2 in list2:
+        list2_counts[_l2] += 1
+
+    score = 0
+    for _l1 in list1:
+        score += _l1 * list2_counts.get(_l1, 0)
+
+    return score
+
+
 if __name__ == "__main__":
     input_file = Path("input.txt")
     with input_file.open("r") as f:
         input_str = f.read()
     input_data = parse_input(input_str)
     print(f"Solution 1: {compare_lists(*input_data)}")
+    print(f"Solution 2: {similarity_score(*input_data)}")
